24a925c7001c9fd6fa4d41b44f08c0c6121a2ba1
Henrique Kriguer <henriquekriguer@gmail.com>
2023-06-26 12:25:18 -0300

    function callback

---
+++ 22-callBack.js
@@ -0,0 +1,40 @@
+function rand(min = 1000, max = 3000){
+    const num = Math.random()*(max-min) + min
+    return Math.floor(num);
+}
+
+function f1(callback){
+    setTimeout(function() {
+        console.log('f1');
+        if (callback) callback();
+    }, rand());
+}
+function f2(callback){
+    setTimeout(function() {
+        console.log('f2');
+        if (callback) callback();
+    }, rand());
+}
+function f3(callback){
+    setTimeout(function() {
+        console.log('f3');
+        if (callback) callback();
+    }, rand());
+}
+f1();
+f2();
+f3();
+console.log('olá mundo!');
+
+// veja que executando o codigo da forma aleatória, a cada execução a ordem é aleatoria para f1,f2,f3 e 
+// o console.log sempre executa primeiro . 
+// a seguir uma forma de encadear a execução de cada função na ordem pré estabelecida,
+//  independente do tempo que cada uma vai ser executada.
+//f1(function(){
+//    f2(function(){
+//        f3(function(){
+//            console.log('Ola Mundo');
+//        })
+//    })
+//});
+
